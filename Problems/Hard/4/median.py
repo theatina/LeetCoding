d652d3defa8297893b7ebf16229e56ca6008fa98
Theatina <ct.kylafi@gmail.com>
2021-03-15 14:25:12 +0200

    update

--- Problems/Hard/4/median.py
+++ Problems/Hard/4/median.py
@@ -1,19 +1,13 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-
-        median = 0.0
-        
-        nums1.extend(nums2)
-        # nums1 = list(set(nums1))
-        nums1 = [x for x in nums1 if x < 0] + [x for x in nums1 if x >= 0] 
-        nums1.sort()
-        
-        if (len( nums1)/2).is_integer():
-          middle = int(len(nums1)/2)
-          median = float(nums1[middle-1]+nums1[middle])/2
-        else:
-          middle = int(len(nums1)/2)
-          median = nums1[middle]
-
-        
-        return median
+  def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    median = None
+    
+    final_list = nums1+nums2
+    final_list.sort()
+    
+    if (len( final_list)/2).is_integer():
+        median = (final_list[len(final_list)//2-1]+final_list[len(final_list)//2])/2
+    else:
+        median = final_list[len(final_list)//2]
+            
+    return median
