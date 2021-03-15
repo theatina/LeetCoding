3d9d0318bb65f4716b5d0f1dc652521de7dbeb15
Theatina <ct.kylafi@gmail.com>
2021-03-15 14:26:58 +0200

    update

--- Problems/Hard/4/median.py
+++ Problems/Hard/4/median.py
@@ -5,9 +5,10 @@ class Solution:
     final_list = nums1+nums2
     final_list.sort()
     
+    middle = len(final_list)//2
     if (len( final_list)/2).is_integer():
-        median = (final_list[len(final_list)//2-1]+final_list[len(final_list)//2])/2
+        median = (final_list[middle-1]+final_list[middle])/2
     else:
-        median = final_list[len(final_list)//2]
+        median = final_list[middle]
             
     return median
